4211414d6cf21848d2262332b04caab5b15ce8d5
Murali Elavarthi <muralis2p@gmail.com>
2025-06-21 14:09:09 +0530

    m1

--- 2.sh
+++ 2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 TIMESTAMP_DATE=$(date +"%d-%m-%y")
 
-TIMESTAMP_TIME=$(date +"H-%M-%S")
+TIMESTAMP_TIME=$(date +"%H-%M-%S")
 
 echo "DATEis $TIMESTAMP_DATE and TIME IS $TIMESTAMP_TIME"
 
