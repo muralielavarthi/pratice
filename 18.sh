132af577d5beaebd9451e7049c1e5fea6b48921b
Murali Elavarthi <muralis2p@gmail.com>
2025-06-21 14:33:16 +0530

    m1

--- 18.sh
+++ 18.sh
@@ -2,14 +2,17 @@
 
 function sum()
 {
+    num2=20
     RESULT=$(($num1+$num2))
     echo "result is $RESULT"
+    # num1 value is from global varibale
+    # num2 value is from local variable
 }
 
 echo "enter num1"
-read num1
+read num1 # global variable
 
 echo "enter num2"
-read num2
+read num2 # global veriable
 
 sum
