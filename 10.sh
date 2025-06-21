09bd34151e413962f40dcec0b90704f7667532e6
Murali Elavarthi <muralis2p@gmail.com>
2025-06-21 14:18:49 +0530

    m1

--- 10.sh
+++ 10.sh
@@ -4,10 +4,16 @@ echo "creating dummy directory...."
 
 mkdir dummy
 
+echo "entering inside dummy"
+
 cd dummy
 
 for i in {1..5}
 do
-    touch "file-$i"
+    touch "file-txt-$i"
 done
+
+echo "created file names"
+
+ls -l
 
