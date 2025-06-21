26f03a63247cee63da5bc895e000c401b5b6bde6
Murali Elavarthi <muralis2p@gmail.com>
2025-06-21 15:11:45 +0530

    m1

--- 19.sh
+++ 19.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
-echo "Enter DIR path:"
-read DIR_PATH
+echo "enter the directory path"
 
-if [ -d "$DIR_PATH" ]; then
-    echo "Directory path is valid, checking error files"
+read $DIR_PATH
+
+if [ -d "$DIR_PATH" ]
+then
+    echo "valid directory, continuing..."
 else
-    echo "Directory path invalid, exiting!"
-    exit 1
+    echo "invalid director, exited!"
 fi
 
-echo "Finding text files"
-OUTPUT_FILE="output.txt"
-> "$OUTPUT_FILE"  # This truncates or creates the file
-echo "Created final output file"
+echo "creating output file"
+touch output_file.txt
 
-# Store the result of find command in a variable safely using command substitution
+echo "finding text files containing error string"
+
 TEXT_FILES_LIST=$(find "$DIR_PATH" -type f -name "file*")
 
-# Iterate over each file
-for FILE in $TEXT_FILES_LIST; do
-    if grep -qi "error" "$FILE"; then
-        echo "$FILE" >> "$OUTPUT_FILE"
+for file in $TEXT_FILES_LIST
+do
+    if grep -i "error" $file
+    then
+        echo $file>>output_file.txt
     fi
 done
 
-echo "Text files containing the word 'error':"
-cat "$OUTPUT_FILE"
+echo "printing file containing error string"
+
+cat output_file.txt
