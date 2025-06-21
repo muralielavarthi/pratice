b7b9b82e6169278eaac673ce8a239f2c04bbce56
Murali Elavarthi <muralis2p@gmail.com>
2025-06-21 14:14:15 +0530

    m1

--- 6.sh
+++ 6.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-CURRENT_DATE=$(date +"%d")
+echo "enter date"
+
+read i
+
+CURRENT_DATE=$i
 
 if [ $(($CURRENT_DATE%2)) -eq 0 ]
 then
     echo "Current date: $CURRENT_DATE is even number"
+else
+    echo "not even day"
 fi
 
